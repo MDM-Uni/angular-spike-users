f58d576b235c0e8ac48d1320fb4a79093e16bd83
Davide Zago <davide.zago@edu.unito.it>
2021-12-16 16:56:47 +0100

    Mofifica index e componente root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { UserList } from './user-list/user-list.component';
+import {UsersService} from "./service/users.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserList
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    UsersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
